92133e7b30ffa8cb6f2f7369c0df6ab9cfe2582e
Micah Cliffe <micah.cliffe@ucla.edu>
2017-03-15 19:50:41 -0700

    Update DotJpeg.v

--- DotJpeg.v
+++ DotJpeg.v
@@ -1,14 +1,14 @@
 `timescale 1ns / 1ps
 ////////////////////////////////////////////////////////////////////////////////
 // Company:  Swag L0rds Inc.
-// Engineer: Elon Musk
+// Engineer: Ghandi
 // 
 // Create Date:    12:13:04 02/27/2017 
 // Design Name:    Pretty
 // Module Name:    DotJpeg 
-// Project Name:   Nim, bitch
+// Project Name:   Nim
 // Target Devices: Nexys 3
-// Tool versions:  69
+// Tool versions:  0
 // Description:    Play th game
 //
 // Dependencies:   Friends
@@ -22,13 +22,13 @@
 // This is the tippiest toppiest level bruhh
 module DotJpeg(
     input wire clk,
-    input wire Rst,
+    input wire Rst,            // unused
     inout [7:0] JA,
-    output wire [2:0] vgaRed,	//red vga output - 3 bits
-	output wire [2:0] vgaGreen, //green vga output - 3 bits
-	output wire [1:0] vgaBlue,	//blue vga output - 2 bits
-	output wire Hsync,		    //horizontal sync out
-	output wire Vsync			//vertical sync out
+    output wire [2:0] vgaRed,   //red vga output - 3 bits
+    output wire [2:0] vgaGreen, //green vga output - 3 bits
+    output wire [1:0] vgaBlue,  //blue vga output - 2 bits
+    output wire Hsync,	        //horizontal sync out
+    output wire Vsync	        //vertical sync out
     );
     
     wire dclk;
@@ -36,7 +36,7 @@ module DotJpeg(
     wire [3:0] key;
     wire [160:0] text;
     
-	PespsiCola PC (
+    PespsiCola PC (
         .clk(clk),
         .dclk(dclk)
     );
